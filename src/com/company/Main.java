24b3cf335591016f57a293ec3c5adc44f8ed7360
Rg12rg34 <rahulguptamca1@gmail.com>
2021-09-10 14:05:58 +0530

    solving using switch case statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
 
         Random random = new Random();
         int empCheck = random.nextInt(2);
+
+
         if (empCheck == IS_FULL_TIME){
-            System.out.println("Employee is Present");
             empHours = 8;
         }
+        else if (empCheck == IS_PART_TIME){
+            empHours = 4;
+        }
         else {
-            System.out.println("Employee is Absent");
             empHours = 0;
         }
         empWage = empHours * EMP_RATE_PER_HOUR;
